e88c64d5e7c401d4f5d3eb9f84c0391ae9ff1bce
davisllv <davisllvsnt57@gmail.com>
2022-04-20 21:20:08 -0300

    string

---
+++ 4_string/scripts.js
@@ -0,0 +1,8 @@
+console.log("Oi meu nome é Davi da Silva");
+
+const nome = "Davi";
+const sobrenome = "da Silva dos Santos";
+
+console.log(`Oi meu nome é ${nome} ${sobrenome}`);
+
+// Template literals é um resumo da concatenação do próprio js, consigo colocar variaves de forma mais fácil
